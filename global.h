6294c43506e4128b0094583cf02fa5519726747d
harryrz <runzezhao09@gmail.com>
2024-03-29 15:17:37 -0400

    Created crystal data structures and initialized crystal rendering pipeline

--- global.h
+++ global.h
@@ -9,3 +9,20 @@
 
 int pixel_buffer_start; // global variable
 
+struct Crystal{
+    int shapeType; // Type of the rock, denoted by an integet number: Diamond, Rock, Sapphire etc.
+    short int colour; // Colour of the crystal
+    int price; // Price of the crystal
+    int** x_loc_list; // Pointer to the start drawing location of the crystal
+    int** y_loc_list; // Pointer to the end drawing location of the crystal
+    int pixel_size; // How many pixels do we want to draw for this crystal
+};
+
+short int colourList [10] = {0xa5b6, 0x9fb3, 0xc21e, 0x5d9d, 0x0540, 0xf925, 0xf3bd, 0x213d, 0xfda5, 0xffbe};
+// In order: Rock, Periodot, Amethyst, Aqua, Emerald, Ruby, Rose, Sapphire, Gold, Diamond
+
+int shapeList [10] = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
+
+
+
+
--- goldminer.c
+++ goldminer.c
@@ -3,15 +3,34 @@
 #include <stdbool.h>
 #include <math.h>
 #include "global.h"
+#include "vga_display.c"
 
 //Define some variables:
 #define RLEDs ((volatile long *) 0xFF200000)
+#define numCrystals 15 // Defines how many crystals we want to render on the picture.
 
 
 
 
 
 int main() {
+
+    struct Crystal ** crystalList;
+    crystalList = (struct Crystal **)malloc(numCrystals * sizeof(struct Crystal *));
+    for(int i = 0; i < numCrystals; i++){
+        crystalList[i] = (struct Crystal *)malloc(sizeof(struct Crystal));
+    }
+
+
+
+    set_crystals(crystalList); // This populates the crystalList array with random parameters
+    
+
+
+
+
+
+
 	unsigned char byte1 = 0;
 	unsigned char byte2 = 0;
 	unsigned char byte3 = 0;
@@ -40,29 +59,37 @@ int main() {
 	}
 }
 
-bool detect_hook_on_object(int hookx, int hooky, double slope){
+bool detect_hook_on_object(int hookx, int hooky){
     volatile short int * check_pixel_address;
-    
-    if(0 < slope && slope < 1){
-        int newhookx = hookx + 1; // Move x right by 1
-        int newhooky = (int)round(hooky + slope); // Move y down accordingly
-        check_pixel_address = pixel_buffer_start + (newhooky << 10) + (newhookx << 1);
-    }
-    else if(slope >= 1){
-        int newhooky = hooky + 1; // move y down by 1
-        int newhookx = (int)round(hookx + slope); // move x right accordingly
-        check_pixel_address = pixel_buffer_start + (newhooky << 10) + (newhookx << 1);
-    } 
-    else if( slope < 0 && slope > -1){
-        int newhookx =  hookx - 1; // Move x left by 1
-        int newhooky = (int)round(hooky - slope); // move y down accordingly
-        check_pixel_address = pixel_buffer_start + (newhooky << 10) + (newhookx << 1);
-    }
-    else{
-        int newhooky = hooky + 1;
-        int newhookx = (int)round(hookx + slope); // move x left accordingly
-        check_pixel_address = pixel_buffer_start + (newhooky << 10) + (newhookx << 1);
-    }
+    check_pixel_address = pixel_buffer_start + (hooky << 10) + (hookx << 1);
+    // We now check the pixel of the "soon to be drawn if not detected" hook head.
+
+    // Algorithm: We check if the imaginary hook head is black or not,
+    // if it is black, meaning at that position, there is not crystal, thus we keep drawing the hook
+    // if it is not black, meaning at that position, there is a crystal, we starat invoking the retrive_hook_rock function.
+
+    // We check the pixel of hook
+    // if(0 < slope && slope < 1){
+    //     int newhookx = hookx + 1; // Move x right by 1
+    //     int newhooky = (int)round(hooky + slope); // Move y down accordingly
+    //     check_pixel_address = pixel_buffer_start + (newhooky << 10) + (newhookx << 1);
+    // }
+    // else if(slope >= 1){
+    //     int newhooky = hooky + 1; // move y down by 1
+    //     int newhookx = (int)round(hookx + slope); // move x right accordingly
+    //     check_pixel_address = pixel_buffer_start + (newhooky << 10) + (newhookx << 1);
+    // } 
+    // else if( slope < 0 && slope > -1){
+    //     int newhookx =  hookx - 1; // Move x left by 1
+    //     int newhooky = (int)round(hooky - slope); // move y down accordingly
+    //     check_pixel_address = pixel_buffer_start + (newhooky << 10) + (newhookx << 1);
+    // }
+    // else{
+    //     int newhooky = hooky + 1;
+    //     int newhookx = (int)round(hookx + slope); // move x left accordingly
+    //     check_pixel_address = pixel_buffer_start + (newhooky << 10) + (newhookx << 1);
+    // }
+
 
     if(*check_pixel_address != 0x0000){ // The "Next" pixel of the current pixel is not black, indicating
                                         // that a rock/gold has been reached.
@@ -72,4 +99,28 @@ bool detect_hook_on_object(int hookx, int hooky, double slope){
     }
 }
 
+void set_crystals( struct Crystal ** crystalList){
+    for(int i = 0; i < numCrystals; i++){
+        int crystal_index = colourList[rand()%10];
+        crystalList[i]->colour = colourList[crystal_index];
+        //crystalList[i]->start_x_loc = 20 + rand()%260; //Leave some padding for x
+        //crystalList[i]->start_y_loc = 20 + rand()%180; //Leave some padding for y
+        crystalList[i]->shapeType = shapeList[crystal_index];
+        crystalList[i]->price = crystal_index * 100 + (int)colourList[crystal_index];
+    }
+}
 
+void draw_crystals( struct Crystal ** crystalList){
+    for(int i = 0; i < numCrystals; i++){
+        if(crystalList[i]->shapeType == 1){ // Square Type
+            crystalList[i]->pixel_size = 16;
+            crystalList[i]->x_loc_list = (int *)malloc(crystalList[i]->pixel_size * sizeof(int));
+            crystalList[i]->y_loc_list = (int *)malloc(crystalList[i]->pixel_size * sizeof(int));
+            crystalList[i]->x_loc_list[0] = 20 + rand()%260;
+            crystalList[i]->y_loc_list[0] = 20 + rand()%180;
+
+            
+
+        }
+    }
+}
